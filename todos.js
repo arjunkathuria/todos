429bececd26ab4dd54ca62ad4b6b9e96801bff31
Arjun kathuria <arjun.kathuria8@gmail.com>
2015-08-01 01:34:48 +0530

    adds basic functionality

--- todos.js
+++ todos.js
@@ -1,8 +1,40 @@
+Todos = new Meteor.Collection('todos');
+
 if (Meteor.isClient) {
-  //client side code goes here
+  Template.todos.helpers({
+    todo: function(){
+      return Todos.find({},{sort: {createdAt: -1}});
+    }
+  })
+
+  Template.addTodo.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      var todoName=$('[name="todoName"]').val()
+      Todos.insert({
+        'name': todoName,
+        'completed':false,
+        'createdAt': new Date()
+      }); 
+
+      var todoName=$('[name="todoName"]').val('')
+
+    }
+  })
+
+  Template.todoItem.events({
+    'click .todo-delete': function (event) {
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("You sure bruh ?")
+      if (confirm){
+
+        Todos.remove({_id: documentId}) ;
+      }
+    }
+  })
 }
 
 if (Meteor.isServer) {
   //server side code goes here
 }
-
